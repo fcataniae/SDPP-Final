9f673e7e49d3114454ace466c52ea6042f321bb3
fcataniae <cataniafrane@gmail.com>
2019-08-04 00:27:16 -0300

    agrega configuraciones y estilos upgrade version de angular cli

--- node/frontend/src/app/app.module.ts
+++ node/frontend/src/app/app.module.ts
@@ -1,8 +1,5 @@
-
-
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConfigurationComponent } from './components/configuration/configuration.component';
@@ -10,16 +7,20 @@ import { APP_BASE_HREF, LocationStrategy } from '@angular/common';
 import { CustomLocationStrategy } from './app.common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent,
     ConfigurationComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    MatInputModule
   ],
   providers: [
     {
