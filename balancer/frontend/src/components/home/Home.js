5a35f7b096dd32b459519dfb854cba1fb665fdeb
fcataniae <cataniafrane@gmail.com>
2019-08-20 09:42:25 -0300

    agrega logo .svg

--- balancer/frontend/src/components/home/Home.js
+++ balancer/frontend/src/components/home/Home.js
@@ -1,12 +1,16 @@
-import React from "react";
-import SearchText from "../searchtext/SearchText";
+import React from 'react';
+import SearchText from '../searchtext/SearchText';
 import './Home.css';
+import logo from '../../logo.svg';
 
 export default class Home extends React.Component {
 
     render(){
         return (
             <div className="home">
+                <div className="ml-auto mr-auto p-xl-3" >
+                    <img className=" rounded mx-auto d-block " src={logo} alt="logo"/>
+                </div>
                 <SearchText/>
             </div>
         );
